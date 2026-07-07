ecde38481fea77cb035e2563cfd7e7d87b23aca1
Liliia <plv-86@ukr.net>
2026-07-07 17:36:09 +0300

    second commit mongodb

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -45,7 +45,7 @@ export const updateNote = async (req, res, next) => {
       noteId,
       req.body,
       {
-        new: true,
+        returnDocument: 'after',
       },
     );
 
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,14 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      message: err.message,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    message: 'Something went wrong',
   });
 };
