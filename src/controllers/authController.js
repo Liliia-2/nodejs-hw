e8db7b8e9fe67484a58f542e9c8036210ee92087
Liliia <plv-86@ukr.net>
2026-07-23 18:47:18 +0300

    fourth commit he-04

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -75,14 +75,15 @@ export const refreshUserSession = async (req, res) => {
       _id: session._id
     });
 
-
     res.clearCookie('sessionId');
     res.clearCookie('accessToken');
     res.clearCookie('refreshToken');
 
     throw createHttpError(401, 'Session token expired');
   }
-
+  await Session.deleteOne({
+    _id: session._id,
+  });
 
   const newSession = await createSession(session.userId);
 
