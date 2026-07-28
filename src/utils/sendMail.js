d75d46f4c97f052388f5480dcf344b3c31dfc1d7
Liliia <plv-86@ukr.net>
2026-07-28 23:26:03 +0300

    fourth commit hw-05

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -15,10 +15,7 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-  return transporter.sendMail({
-    from: process.env.SMTP_FROM,
-    ...options,
-  });
+  return transporter.sendMail(options);
 };
 
 
